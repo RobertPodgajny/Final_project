4f1f8cb0ea084f322b6c9b1928b4496996233582
Robert Podgajny <robertpodgajny@gmail.com>
2018-04-09 17:17:51 +0200

    change display, add basket: first step

--- handicraft_shop/shop_app/models.py
+++ handicraft_shop/shop_app/models.py
@@ -66,12 +66,6 @@ class Cushion(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField(null=True, blank=True)
 
-    # def __str__(self):
-    #     return "{}".format(self.get_kind_display())
-
-    ## cusion = Cushion.objects.get*(
-    ## <img src="{{ cusion.image.url }}" />
-
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
